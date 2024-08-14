b95f1dc8badbdf6c726e3b529f907c7043baf77e
Kei-1111 <kei_02_1111@icloud.com>
2024-08-14 23:24:46 +0900

    first commit

---
+++ composeApp/src/wasmJsMain/kotlin/org/example/project/main.kt
@@ -0,0 +1,15 @@
+package org.example.project
+
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.window.ComposeViewport
+import kotlinx.browser.document
+
+@OptIn(ExperimentalComposeUiApi::class)
+fun main() {
+//    ComposeViewport(viewportContainerId = "composeApplication") {
+//        App()
+//    }
+    ComposeViewport(document.body!!) {
+        App()
+    }
+}
